6dd68ae8af5068a47a1d93c6f65d76aa2728f721
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2020-06-28 10:25:46 +0530

    lets see first commit

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
 	mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@developerslane-p3o4t.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-	secret: process.env.JWT_KEY,
+	secret: process.env.secret,
 };
